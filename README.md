5b4e27261b9c4b8494199a09d58fc31e51411996
skingi20 <skingi20@student.aau.dk>
2023-05-12 14:04:28 +0200

    initial_commit

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# A better split of PharmKG8k
+
+In this GitHub repository an improved split of the PharmKG8k dataset can be found. In the original split found on their [GitHub repository](https://github.com/biomed-AI/PharmKG) several issues exists.
+
+## Issues in the original split
+
+These issues consists of:
+1. Duplicates in each individual set, train, test and valid.
+2. head, relation, tail combinations existing in both e.g. train and test.
+3. head, tail combinations existing in both e.g. train and test.
+4. tail, head combinations in e.g. train existing as head, tail in e.g. test.
+Proof of these issues can be found in LINK, and the code to generate these files can be found in *investigation_of_duplicates_in_pharmkg8k.py*
+
+## The better split
+This new split have some different goals in mind when creating it. These can be expressed as a math equation. The denotions we use are as mentioned:
+
+$$P_tr = train$$
+
+$P_ts = test$
+
+$P_va = valid$
+
+Then the rules can be expressed as such:
+
+$$\forall (h,r,t) \in P_{tr} : \neg(\exists (h',t') \in (P_{ts} \cup P_{va}) : \\
+((h = h' \land t = t') \lor (h = t' \land t = h')))$$
+
+*and*
+
+$$\forall (h,r,t) \in P_{ts} : \neg(\exists (h',t') \in (P_{tr} \cup P_{va}) : \\
+((h = h' \land t = t') \lor (h = t' \land t = h')))$$
+
+*and*
+
+$$\forall (h,r,t) \in P_{va} : \neg(\exists (h',t') \in (P_{tr} \cup P_{ts}) : \\
+((h = h' \land t = t') \lor (h = t' \land t = h')))$$
+
+In other words, the new split is created such that for all edges in the training set there does not exist an edge in either the test set nor the valid set, where the head and the tail is equivalent to the head and the tail in the training set. Nor does there exist edges where the head and tail is equivalent to the tail and head in the training set.
+Vice versa for the test and valid sets.
+
+This split can be found in LINK and the code to generate these can be found in *making_the_split_files.py*. It should be noted that this code is not optimized so it may take several hours to run.
---
+++ making_the_split_files.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+base_url = 'https://raw.githubusercontent.com/biomed-AI/PharmKG/master/data/PharmKG-8k/'
+
+train = pd.read_table(base_url+'train.tsv', names=["head", "relation", "tail"])
+test = pd.read_table(base_url+'test.tsv', names=["head", "relation", "tail"])
+valid = pd.read_table(base_url+'valid.tsv', names=["head", "relation", "tail"])
+
+dataset = train.append(test).append(valid).drop_duplicates()
+
+#Setting size of each split
+train_size = int(len(dataset)*0.8)
+test_size = int(len(dataset)*0.1)
+valid_size = int(len(dataset)*0.1)
+
+def get_split_for_dataframe(dataframe, split_size):
+    """
+    get_split_for_dataframe takes a graph as a dataframe and an amount 
+    of edges decided by split_size and returns a dataframe. In this dataframe
+    it is made sure that if a "head","tail" is added to the dataframe, any 
+    other edges with this same "head","tail" or a symmetric edge with the same
+    "tail","head" is also added to the return dataframe.
+
+    :param dataframe: A pandas dataframe with the header "head","relation","tail"
+    :param split_size: An integer deciding how many edges is wanted in the new dataframe, 
+    has to be less or equal to the dataframe size
+    :return: A pandas dataframe with the header "head","relation","tail"
+    """
+    moved_pairs = set()
+    split_dataframe = set()
+    pharm_kg_no_relation = dataframe[['head','tail']]
+    pharm_kg_list = list(pharm_kg_no_relation.itertuples(index=False, name=None))
+    i = - 1
+    for ind in dataframe.index:
+        i = i+1
+        if i%50 == 0:
+            print(f'{i} out of {split_size} done')
+        head_tail = (dataframe.iat[ind,0],dataframe.iat[ind,2])
+        head_relation_tail = (dataframe.iat[ind,0],dataframe.iat[ind,1],dataframe.iat[ind,2])
+        tail_head = (dataframe.iat[ind,2],dataframe.iat[ind,0])
+        if head_tail in moved_pairs or tail_head in moved_pairs:
+            continue
+        split_dataframe.add(head_relation_tail)
+        moved_pairs.add(head_tail)
+        moved_pairs.add(tail_head)
+        indexes = [index for index, value in enumerate(pharm_kg_list) if value == head_tail]
+        for index in indexes:
+            split_dataframe.add((dataframe.iat[index,0],dataframe.iat[index,1],dataframe.iat[index,2]))
+        indexes = [index for index, value in enumerate(pharm_kg_list) if value == tail_head]
+        for index in indexes:
+            split_dataframe.add((dataframe.iat[index,0],dataframe.iat[index,1],dataframe.iat[index,2]))
+        if len(split_dataframe) >= split_size:
+            break
+    df = pd.DataFrame(split_dataframe, columns =['head', 'relation', 'tail'])
+    return df
+
+
+new_train = get_split_for_dataframe(dataset, train_size)
+
+dataset_no_train = dataset.append(new_train).drop_duplicates(keep=False).reset_index(drop=True)
+
+new_test = get_split_for_dataframe(dataset_no_train, test_size)
+
+new_valid = dataset_no_train.append(new_test).drop_duplicates(keep=False)
+
+new_train.to_csv('improved_split/new_train.tsv', sep="\t", index = False)
+
+new_test.to_csv('improved_split/new_test.tsv', sep="\t", index = False)
+
+new_valid.to_csv('improved_split/new_valid.tsv', sep="\t", index = False)
